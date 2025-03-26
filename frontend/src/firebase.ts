1d453c34bff778c795b943a01c834df347d8024e
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-03-26 21:32:34 +0530

    latest

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword  } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC8GpOjxS8weyQRrYefvzyOXR8rHmyCFtE",
-    authDomain: "ecommerce-a9b5e.firebaseapp.com",
-    databaseURL: "https://ecommerce-a9b5e-default-rtdb.firebaseio.com",
-    projectId: "ecommerce-a9b5e",
-    storageBucket: "ecommerce-a9b5e.firebasestorage.app",
-    messagingSenderId: "510614618094",
-    appId: "1:510614618094:web:a9574ca506e6d90f73d8ff",
-    measurementId: "G-X6ZHDWCRQT"
-  };
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export { createUserWithEmailAndPassword ,signInWithEmailAndPassword };
+export { createUserWithEmailAndPassword, signInWithEmailAndPassword };
